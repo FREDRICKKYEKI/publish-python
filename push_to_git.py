c70184eee86ae668b27f1b1b1997c5264411f93e
Fredrick Kyeki <fredrickisaac142@gmail.com>
2024-03-14 21:26:09 +0300

    '{random_string}'

--- push_to_git.py
+++ push_to_git.py
@@ -3,7 +3,7 @@
 This script is used to push the changes to the git repository in CSV format.
 """
 import os
-import time
+import secrets
 
 
 def push_to_git():
@@ -11,9 +11,10 @@ def push_to_git():
     This function is used to push the changes to the git repository.
     """
     os.system("git add .")
+    random_string = secrets.token_hex(16)
     # commit_message = f"Updated files at {
     #     time.strftime('%a %b %d %H:%M:%S %Y')}"
-    os.system("git commit -m 'commit_message'")
+    os.system("git commit -m '{random_string}'")
     # Assuming the branch is 'main', change if necessary
     os.system("git push origin main")
     print("[INFO] --- Pushed the changes to the git repository. ---")
