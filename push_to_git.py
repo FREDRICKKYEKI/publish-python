12aa7548aa067a05843a8f8c879ddd993c3a7f33
Fredrick Kyeki <fredrickisaac142@gmail.com>
2024-03-14 21:23:18 +0300

    'commit_message'

--- push_to_git.py
+++ push_to_git.py
@@ -11,6 +11,13 @@ def push_to_git():
     This function is used to push the changes to the git repository.
     """
     os.system("git add .")
-    os.system(f"git commit -m 'Updated the files at {time.ctime()}'")
-    os.system("git push origin master")
+    # commit_message = f"Updated files at {
+    #     time.strftime('%a %b %d %H:%M:%S %Y')}"
+    os.system("git commit -m 'commit_message'")
+    # Assuming the branch is 'main', change if necessary
+    os.system("git push origin main")
     print("[INFO] --- Pushed the changes to the git repository. ---")
+
+
+if __name__ == "__main__":
+    push_to_git()
